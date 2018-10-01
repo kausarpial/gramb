bb28a8b139f6d27a983e7efd56ea5d36423b2b67
Kausar Ahmed <kausarahmedpial@gmail.com>
2018-10-01 23:30:29 +0600

    Added Service Pages

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     //  Headroom Initialize
     // -----------------------------
     $('.main-nav').headroom();
-        
+
     $('.step__item').matchHeight({
         byRow: 0
     });
@@ -20,7 +20,16 @@ $(document).ready(function () {
             $('.navbar-toggler').click();
         }
     });
-    
+
+    // testimonial slider
+    $('.testimonial__slider').slick({
+        arrows: true,
+        autoplay: false,
+        dots: false,
+        prevArrow: '<button type=\'button\' class=\'prevArrow\'><i class=\'ti-angle-left\'></i></button>',
+        nextArrow: '<button type=\'button\' class=\'nextArrow\'><i class=\'ti-angle-right\'></i></button>'
+    });
+
     // -----------------------------
     //  Parallax Scene
     // -----------------------------
@@ -28,7 +37,7 @@ $(document).ready(function () {
     //     var scene = $('#icons').get(0);
     //     var parallaxInstance = new Parallax(scene);
     //     parallaxInstance.friction(0.2, 0.2);
-    
+
     //     var scene2 = $('#audienceIcons').get(0);
     //     var parallaxInstance2 = new Parallax(scene2);
     //     parallaxInstance2.friction(0.2, 0.2);
@@ -42,15 +51,17 @@ $(document).ready(function () {
     // -----------------------------
     //  Smooth Scrollbar
     // -----------------------------
-    var Scrollbar = window.Scrollbar;
-    Scrollbar.init(document.querySelector('#my-scrollbar'));
-});
+    // var Scrollbar = window.Scrollbar;
+    // Scrollbar.init(document.querySelector('#my-scrollbar'));
 
-$(window).on('scroll', function () {
-    var windscroll = $(window).scrollTop();
-    if (windscroll >= 100) {
-        $('.main-nav').addClass('nav-bg');
-    } else {
-        $('.main-nav').removeClass('nav-bg');
-    }
+    $(window).on('scroll', function () {
+        var windscroll = $(window).scrollTop();
+        if (windscroll >= 100) {
+            $('.main-nav').addClass('nav-bg');
+        } else {
+            $('.main-nav').removeClass('nav-bg');
+        }
+    });
+
+
 });
